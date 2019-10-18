788e2044b1bb38e4749fc832eb2d4c5c62f79ae8
nataliedowen <Enter your email address>
2019-10-17 22:29:11 -0600

    go

--- lesson5/js/toggle.js
+++ lesson5/js/toggle.js
@@ -2,3 +2,8 @@ function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
+  function showhide() {
+    var d = new Date();
+    var s = document.getElementById(+d.getDay());
+    s.style.display = (s.style.display == 'block') ? 'none' : 'block';
+  }
