c7403083a4ae7ac60a01595cffdbeb359b3a4d70
nataliedowen <Enter your email address>
2019-11-07 20:56:37 -0700

    go

--- lesson6/js/toggle.js
+++ lesson6/js/toggle.js
@@ -14,7 +14,22 @@ function toggleMenu() {
 }
 
   function showhide() {
+  /*///
     var d = new Date();
     var s = document.getElementById(+d.getDay());
     s.style.display = (s.style.display == 'block') ? 'none' : 'block';
-  }
+    /*///
+
+      var x = document.getElementById("5");
+      var day = weekday[now.getDay()];
+      if (day == weekday[3]) {
+        x.style.display = "block";
+      } 
+      else {
+        x.style.display = "none";
+      }
+    }
+    
+
+
+ 
