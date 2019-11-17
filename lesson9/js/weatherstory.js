01e913a31959dda2ba8eb10e3f8c57505c586ae0
nataliedowen <Enter your email address>
2019-11-16 20:42:03 -0700

    finished home page

--- lesson9/js/weatherstory.js
+++ lesson9/js/weatherstory.js
@@ -13,7 +13,7 @@ fetch(requestURL)
       if(i === 1 || i === 4 || i === 5) {
         let card = document.createElement('section');
         let name = document.createElement('h2');
-        let motto = document.createElement('p');
+        let motto = document.createElement('h4');
         let yearFounded = document.createElement('p');
         let currentPopulation = document.createElement('p');
         let averageRainfall = document.createElement('p');
@@ -21,17 +21,18 @@ fetch(requestURL)
         
         name.textContent = towns[i].name;
         motto.textContent = towns[i].motto;
-        yearFounded.textContent = towns[i].yearFounded;
-        currentPopulation.textContent = towns[i].currentPopulation;
-        averageRainfall.textContent = towns[i].averageRainfall;
-        image.setAttribute('src', '/images/' + towns[i].photo);
+        yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
+        currentPopulation.textContent = "Current Population: " + towns[i].currentPopulation;
+        averageRainfall.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+        image.setAttribute('src', 'images/' + towns[i].photo);
         
         card.appendChild(name);
         card.appendChild(motto);
+        card.appendChild(image);
         card.appendChild(yearFounded);
         card.appendChild(currentPopulation);
         card.appendChild(averageRainfall);
-        card.appendChild(image);
+     
         
         document.querySelector('div.cards').appendChild(card);
       }
