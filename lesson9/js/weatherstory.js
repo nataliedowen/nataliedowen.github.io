83443e9bfb957ddabf641fe00f8a74aa3d8c2a77
nataliedowen <Enter your email address>
2019-11-16 19:52:37 -0700

    go

--- lesson9/js/weatherstory.js
+++ lesson9/js/weatherstory.js
@@ -1,37 +1,39 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
-     .then(function (response) {
-     return response.json();
- 
-    })
-    .then(function (jsonObject) {
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
+    
     const towns = jsonObject['towns'];
 
-         for (let i = 0; i < towns.length; i++ ) {
-            let card = document.createElement('section');
-            let name = document.createElement('h2');
-            let tagline = document.createElement('h3');
-            let date = document.createElement('p');
-            let population = document.createElement('p');
-            let rain = document.createElement('p');
-            let image = document.createElement('img');
-
-            name.textContent = towns[i].name;
-            tagline.textContent = towns[i].motto; 
-            date.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-            rain.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-            image.setAttribute("src", towns[i].photo);
-
-            card.appendChild(name);
-            card.appendChild(tagline);
-            card.appendChild(image);
-            card.appendChild(date);
-            card.appendChild(population);
-            card.appendChild(rain);
-
-            document.querySelector('div.cards').appendChild(card);
+    for (let i = 0; i < towns.length; i++ ) {
+      if(i === 1 || i === 4 || i === 5) {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let motto = document.createElement('p');
+        let yearFounded = document.createElement('p');
+        let currentPopulation = document.createElement('p');
+        let averageRainfall = document.createElement('p');
+        let image = document.createElement('img');
+        
+        name.textContent = towns[i].name;
+        motto.textContent = towns[i].motto;
+        yearFounded.textContent = towns[i].yearFounded;
+        currentPopulation.textContent = towns[i].currentPopulation;
+        averageRainfall.textContent = towns[i].averageRainfall;
+        image.setAttribute('src', '/images/' + towns[i].photo);
+        
+        card.appendChild(name);
+        card.appendChild(motto);
+        card.appendChild(yearFounded);
+        card.appendChild(currentPopulation);
+        card.appendChild(averageRainfall);
+        card.appendChild(image);
+        
+        document.querySelector('div.cards').appendChild(card);
+      }
     }
-});
+  });
