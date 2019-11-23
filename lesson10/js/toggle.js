0e445375c20eef4d54d2ca2282f7c875cc930d95
nataliedowen <Enter your email address>
2019-11-22 23:21:39 -0700

    fixed banner and 5 day forecast

--- lesson10/js/toggle.js
+++ lesson10/js/toggle.js
@@ -11,19 +11,14 @@ document.getElementById('timestamp').innerHTML = localeDate.format(today);
 
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+};
 
-  function showhide() {
 
-      var x = document.getElementById("5");
-      var day = weekday[now.getDay()];
-      if (day == weekday[5]) {
-        x.style.display = "block";
+var x = document.getElementById("announcment");
+var day = weekday[now.getDay()];
+  if (day == weekday[5]) {
+    x.style.display = "block";
       } 
-      else {
-        x.style.display = "none";
+  else {
+    x.style.display = "none";
       }
-    }
-    
-
- 
--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,40 +1,6 @@
-/*///const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=d52c66367b251e31339fdb175f5737f2";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=9d347f89ba52d481b32f369e448b26a3";
 
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.getElementById('temp').textContent = jsObject.main.temp;
-    document.getElementById('humidity').textContent = jsObject.main.humidity;
-    document.getElementById('current-condition').textContent = jsObject.weather[0].description;;
-    document.getElementById('wind-speed').textContent = jsObject.wind.speed;
-
-
-var temp = jsObject.main.temp;
-var speed = jsObject.wind.speed;
-var exp = Math.pow(speed,0.16);
-var f = 35.74 + (0.6215 * temp) - (35.75 * exp) + (0.4275 * temp * exp);
-
-f.toFixed(2)
-
-document.getElementById("natalie").innerHTML = f.toFixed(2);
-
-console.log(f);
-});
-
-const apiURLf = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=d52c66367b251e31339fdb175f5737f2";
-
-fetch(apiURLf)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-
-});
-/*///
-
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=bf88f267125606d5e4338dcb27547989";
-
-const apiURLf = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=bf88f267125606d5e4338dcb27547989";
+const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=9d347f89ba52d481b32f369e448b26a3";
 
 fetchAPI = (apiURL, num) =>  {
   fetch(apiURL)
@@ -43,16 +9,16 @@ fetchAPI = (apiURL, num) =>  {
 
     switch(num) {
       case 1:
-        this.summary(jsObject);
+        this.summaryBox(jsObject);
         break;
       case 2:
-        this.forecast(jsObject);
+        this.fiveDay(jsObject);
         break;
     }
   });
 }
 
-summary = (jsObject) =>  {
+summaryBox = (jsObject) =>  {
   var mainObj = jsObject.main;
 
   document.getElementById('temp').textContent = jsObject.main.temp;
@@ -73,24 +39,67 @@ document.getElementById("natalie").innerHTML = f.toFixed(2);
 console.log(f);
 }
 
-  
-
-forecast = (jsObject) => {
+fiveDay = (jsObject) => {
   var count = 0;
 
   for(key in jsObject.list) {
     var textCheck = jsObject.list[key].dt_txt;
     if(textCheck.includes("18:00:00")) {
       count++;
+      var varDay = "";
+      var getDayVar = new Date(jsObject.list[key].dt_txt).getDay();
 
+      switch(getDayVar)  {
+        case 1:
+          varDay = "Mon";
+          break;
+        case 2:
+          varDay = "Tues";
+          break;
+        case 3:
+          varDay = "Wed";
+          break;
+        case 4:
+          varDay = "Thur";
+          break;
+        case 5:
+          varDay = "Fri";
+          break;
+        case 6:
+          varDay = "Sat";
+          break;
+        case 0:
+          varDay = "Sun";
+          break;
+      }
+      document.getElementById('forecast' + count).textContent = varDay;
       const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[key].weather[0].icon + '.png';
       const desc = jsObject.list[key].weather[0].description;
 
-      document.getElementById('temp' + count).textContent = jsObject.list[key].main.temp;
+      document.getElementById('temp' + count).textContent = jsObject.list[key].main.temp.toFixed(0);
 
       document.getElementById('weathericon' + count).setAttribute('src', imagesrc);
       document.getElementById('weathericon' + count).setAttribute('alt', desc);
-    }}}
+    }
+  }
+}
+
+checkWindchill = (temp, windSpeed) =>  {
+  let t = (temp <= 50) ? true : false;
+  let s = (windSpeed > 3.0) ? true : false;
+  var chillDiv = document.getElementById("wind-chill");
+
+  if (t && s) {
+
+    let exp = Math.pow(windSpeed, 0.16);
+    let feh = 35.74 + (0.6215 * temp) - (35.75 * exp) + (0.4275 * temp * exp);
+
+    chillDiv.innerHTML = feh.toFixed(0) + ' F';
+
+  } else {
+    chillDiv.innerHTML = 'N/A';
+  }
+}
 
 fetchAPI(apiURL, 1);
-fetchAPI(apiURLf, 2);
+fetchAPI(apiURL2, 2);
