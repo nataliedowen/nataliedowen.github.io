2f1e0545b89073f315483f4500ad7478b95d1a28
nataliedowen <Enter your email address>
2019-11-30 10:48:08 -0700

    final page start

---
+++ lesson11/index/js/weatherstory.js
@@ -0,0 +1,40 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    
+    const towns = jsonObject['towns'];
+
+    for (let i = 0; i < towns.length; i++ ) {
+      if(i === 1 || i === 4 || i === 5) {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let motto = document.createElement('h4');
+        let yearFounded = document.createElement('p');
+        let currentPopulation = document.createElement('p');
+        let averageRainfall = document.createElement('p');
+        let image = document.createElement('img');
+        
+        name.textContent = towns[i].name;
+        motto.textContent = towns[i].motto;
+        yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
+        currentPopulation.textContent = "Current Population: " + towns[i].currentPopulation;
+        averageRainfall.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+        image.setAttribute('src', 'images/' + towns[i].photo);
+        
+        card.appendChild(name);
+        card.appendChild(motto);
+        card.appendChild(image);
+        card.appendChild(yearFounded);
+        card.appendChild(currentPopulation);
+        card.appendChild(averageRainfall);
+     
+        
+        document.querySelector('div.cards').appendChild(card);
+      }
+    }
+  });
---
+++ lesson11/stormcenter/js/toggle.js
@@ -0,0 +1,28 @@
+var today = new Date(); 
+var localeDate = new Intl.DateTimeFormat("en-GB", {
+   weekday: "long",
+   year: "numeric",
+   month: "long",
+   day: "numeric"
+});
+
+document.getElementById('timestamp').innerHTML = localeDate.format(today);
+
+
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+  function showhide() {
+    var d = new Date();
+    var s = document.getElementById(+d.getDay());
+    s.style.display = (s.style.display == 'block') ? 'none' : 'block';
+  }
+
+  function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+  function thanks() {
+    window.location = "thanks.html"
+  }
