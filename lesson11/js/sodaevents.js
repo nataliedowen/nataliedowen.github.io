264188fec5e4fa7eea2aee8549f929200accc641
nataliedowen <Enter your email address>
2019-11-30 13:34:24 -0700

    go

---
+++ lesson11/js/sodaevents.js
@@ -0,0 +1,24 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const cardContainer = document.querySelector("div.events");
+fetch(requestURL)
+    .then(response=>{return response.json()})
+    .then(json=>{
+        const towns = json["towns"];
+        towns.forEach(town=>{
+            
+            if( town.name =="Soda Springs" ) {
+
+            let card = document.createElement("section");
+            let p = document.createElement("p");   
+            let h2 = document.createElement("h2"); 
+
+            h2.textContent = `Upcoming Events`;
+            p.innerHTML = `${town.events[0]}<br>${town.events[1]}<br>${town.events[2]}`;
+
+            card.appendChild(h2);
+            card.appendChild(p);
+            cardContainer.appendChild(card);
+            }
+
+        });
+    });
--- lesson11/js/weatherstory.js
+++ lesson11/js/weatherstory.js
@@ -13,7 +13,7 @@ fetch(requestURL)
       if(i === 1 || i === 4 || i === 5) {
         let card = document.createElement('section');
         let name = document.createElement('h2');
-        let motto = document.createElement('h4');
+        let motto = document.createElement('h3');
         let yearFounded = document.createElement('p');
         let currentPopulation = document.createElement('p');
         let averageRainfall = document.createElement('p');
@@ -24,7 +24,7 @@ fetch(requestURL)
         yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
         currentPopulation.textContent = "Current Population: " + towns[i].currentPopulation;
         averageRainfall.textContent = "Average Rainfall: " + towns[i].averageRainfall;
-        image.setAttribute('src', 'images/' + towns[i].photo);
+        image.setAttribute('src', 'images/' + towns[i].photo, alt="");
         
         card.appendChild(name);
         card.appendChild(motto);
